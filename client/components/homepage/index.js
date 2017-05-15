048c3652926c3f5a66d532c03e7c56b9ee432331
Tito Lins <tito@blinx.com.br>
2017-05-15 00:55:51 -0300

    deleted boiler plate code and started site structure

--- client/components/homepage/index.js
+++ client/components/homepage/index.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import Helmet from 'react-helmet';
-import { Link } from 'react-router';
 import { example, p, link } from './styles';
+
+import Header from '../header';
+import MainPanel from './mainpanel';
 
 export default class Homepage extends Component {
   /*eslint-disable */
@@ -14,22 +16,17 @@ export default class Homepage extends Component {
   render() {
     return <div>
       <Helmet
-        title='Home page'
+        title='Trancoso Vendas e Aluguel'
         meta={[
           {
             property: 'og:title',
-            content: 'Golang Isomorphic React/Hot Reloadable/Redux/Css-Modules Starter Kit'
+            content: 'Aluguel e venda de casas em Trancoso'
           }
         ]} />
-      <h1 className={example}>
-        Hot Reloadable <br />
-        Golang + React + Redux + Css-Modules
-        <br />Isomorphic Starter Kit</h1>
-      <br />
-      <p className={p}>
-        Please take a look at <Link className={link} to='/docs'>usage</Link> page.
-      </p>
-    </div>;
+      <Header />
+      <MainPanel contentId='mainContent'/>
+
+    </div>
   }
 
 }
