aa6b18e2980a1f941641a6b1416be479b852cd13
Tito Lins <tito@blinx.com.br>
2017-05-15 00:56:23 -0300

    added presentation component for logo

---
+++ client/components/logo/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+export default class Logo extends Component {
+  render() {
+    return  (
+      <object type="image/svg+xml" data="/static/images/logo.svg"></object>
+    )
+  }
+}
+
