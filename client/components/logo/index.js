90ef3a610de156bca82d2aa868a69555bac5441d
Tito Lins <tito@blinx.com.br>
2017-05-17 02:50:35 -0300

    built logo

--- client/components/logo/index.js
+++ client/components/logo/index.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
+import { logo, logoText } from './styles';
 
 export default class Logo extends Component {
   render() {
     return  (
-      <object type="image/svg+xml" data="/static/images/logo.svg"></object>
+      <div className={ logo }>
+        <span>
+          <object type="image/svg+xml" data="/static/images/logo.svg"></object>
+        </span>
+        <span className={ logoText }>
+          <h1>Trancoso</h1>
+          <h6>vendas e aluguel</h6>
+        </span>
+      </div>
     )
   }
 }
