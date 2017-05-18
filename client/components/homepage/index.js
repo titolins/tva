8046e505a6b9ebd0039da9e13ec79f22daff7317
Tito Lins <tito@blinx.com.br>
2017-05-18 07:59:58 -0300

    onEnter fetch

--- client/components/homepage/index.js
+++ client/components/homepage/index.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import Helmet from 'react-helmet';
 import { example, p, link } from './styles';
 
+import { addContent } from '../../actions';
+
 import Header from '../header';
 import MainPanel from './mainpanel';
 
@@ -9,7 +11,29 @@ export default class Homepage extends Component {
   /*eslint-disable */
   static onEnter({store, nextState, replaceState, callback}) {
     // Load here any data.
-    callback(); // this call is important, don't forget it
+
+    fetch("/api/houses",{
+      method: 'GET',
+      dataType: 'jsonp'
+    })
+      .then((response) => {
+        response.json();
+        console.log(response);
+        console.log(response.keys());
+        console.log(response.length);
+      }).then((responseJson) => {
+        console.log(responseJson.keys());
+        console.log(responseJson.length);
+        store.dispatch(addContent(responseJson));
+      }).then(() => {
+        callback(); // this call is important, don't forget it
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+
+
+    //
   }
   /*eslint-enable */
 
