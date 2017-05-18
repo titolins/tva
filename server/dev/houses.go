6a713282f9d143e5811366ab7c6da8b8b06fb94d
Tito Lins <tito@blinx.com.br>
2017-05-17 23:46:55 -0300

    extracted dev

---
+++ server/dev/houses.go
@@ -0,0 +1,41 @@
+package dev
+
+import (
+    "github.com/titolins/trancosovendasaluguel/server/models"
+)
+
+var DevData = []models.House{
+    models.House{
+        Category: "1",
+        Featured: true,
+        Capacity: 4,
+        Content: models.TranslatableContent{
+            PT: models.Content{
+                    Name: "casa 1",
+                    Description: "descricao da casa",
+                    Features: []string{
+                        "wifi",
+                        "ar condicionado" }},
+            EN: models.Content{
+                    Name: "house 1",
+                    Description: "house description",
+                    Features: []string{
+                        "wifi",
+                        "air conditioning" }}}},
+    models.House{
+        Category: "1",
+        Featured: true,
+        Capacity: 4,
+        Content: models.TranslatableContent{
+            PT: models.Content{
+                    Name: "casa 2",
+                    Description: "descricao da casa",
+                    Features: []string{
+                        "wifi",
+                        "ar condicionado" } },
+            EN: models.Content{
+                    Name: "house 1",
+                    Description: "house description",
+                    Features: []string{
+                        "wifi",
+                        "air conditioning" }}}}}
